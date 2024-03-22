ed489f98d967c5b1f1ef5b87f60a00ba31458a8d
FARIHA MALIK <163237663+farihamalik098@users.noreply.github.com>
2024-03-22 16:50:10 -0700

    app.ts,app.js.
    A simple calculator in programming.

---
+++ app.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+
+ import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  {message:"Enter second number",type:"number", name:"secondNumber"},
+  {
+    message:"select one of the operators to perform operation ",
+  type:"list",
+  name:"operator",
+  choices:["Addition","substraction","multiplication","division"],
+
+}
+]);
+
+//conditional statement
+if(answer.operator === "Addition"){
+
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "substraction"){
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if(answer.operator === "multiplication"){
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "division"){
+  console.log(answer.firstNumber / answer.secondNumber);
+}else {
+  console.log("please select valid operator")
+}
+  
+
+
+
+
+  
+
+
+
+
+
+  
+
